4b67a9ed5f50193eb8ffb1d7b5fe8e5ea63f7523
george <7jagjag@gmail.com>
2022-07-31 19:51:51 +0200

    del matches

--- src/components/Matches.tsx
+++ src/components/Matches.tsx
@@ -1,4 +1,4 @@
-import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { Button, Table, Tbody, Td, Th, Thead, Tr, useToast } from '@chakra-ui/react'
 import { DateTime } from 'luxon'
 import { useRouter } from 'next/router'
 
@@ -11,7 +11,19 @@ interface Props {
 const Matches = (_: Props) => {
   const router = useRouter()
   const { tournamentId } = router.query
+  const { invalidateQueries } = trpc.useContext()
+  const toast = useToast()
   const matches = trpc.useQuery(['match.getAllByTournamentId', tournamentId as string])
+  const delMatch = trpc.useMutation('match.delete', {
+    onSuccess: () => {
+      invalidateQueries(['match.getAllByTournamentId'])
+      toast({ title: 'Deleted', status: 'success' })
+    },
+    onError: (err) => {
+      toast({ title: 'Something went wrong', description: err.message, status: 'error' })
+    },
+  })
+  const onClickDel = (id: string) => () => delMatch.mutate(id)
 
   return (
     <>
@@ -25,6 +37,7 @@ const Matches = (_: Props) => {
           <Th>status</Th>
           <Th>location</Th>
           <Th>phase</Th>
+          <Th>Delete</Th>
         </Thead>
         <Tbody>
           {matches.data?.map((match) => (
@@ -40,6 +53,18 @@ const Matches = (_: Props) => {
               <Td>{match.status}</Td>
               <Td>{match.location}</Td>
               <Td>{match.phase}</Td>
+              <Td>
+                <Button
+                  isDisabled={delMatch.isLoading}
+                  isLoading={delMatch.isLoading}
+                  onClick={onClickDel(match.id)}
+                  colorScheme="red"
+                  variant="ghost"
+                  size="sm"
+                >
+                  delete
+                </Button>
+              </Td>
             </Tr>
           ))}
         </Tbody>
