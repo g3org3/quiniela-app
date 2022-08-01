8890576a5488ba460bfaccf7e3a967692e9b504f
george <7jagjag@gmail.com>
2022-08-01 14:10:46 +0200

    fix color for pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,9 +20,9 @@ export default class Document extends NextDocument {
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#ffffff" />
 
           <link rel="manifest" href="/manifest.json" />
 
