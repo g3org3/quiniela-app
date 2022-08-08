672f98127aa6f41f5ee3dc9ea58508d695eaa53b
george <7jagjag@gmail.com>
2022-08-08 21:17:08 +0200

    fix types

--- src/server/router/racedriver.ts
+++ src/server/router/racedriver.ts
@@ -1,25 +1,17 @@
 import { RaceDriver, RaceTeam } from '@prisma/client'
-import { z } from 'zod'
+
+import { createCache } from 'utils/cache'
 
 import { createProtectedRouter, isAdminOrThrow } from './protected-router'
 
-type R = (RaceDriver & {
-  raceteam: RaceTeam
-})[]
-const cache: Record<string, R> = {}
-const tap = <T>(obj: T, k: string) => {
-  console.log('<- CACHE-HIT: ', k)
-
-  return obj
+type R = RaceDriver & {
+  raceteam: RaceTeam | null
 }
+const cacheOne = createCache<R[]>('racedriver')
 
 export const racedriverRouter = createProtectedRouter().query('getAll', {
-  async resolve({ ctx }) {
-    console.log('-> racedriver.getAll')
-    if (cache['getAll']) return tap(cache['getAll'], 'racedriver.getAll')
-
-    const data = await ctx.prisma.raceDriver.findMany({ include: { raceteam: true } })
-
-    return (cache['getAll'] = data)
-  },
+  resolve: async ({ ctx }) =>
+    cacheOne.next('getAll', 'getAll', async () => {
+      return await ctx.prisma.raceDriver.findMany({ include: { raceteam: true } })
+    }),
 })
